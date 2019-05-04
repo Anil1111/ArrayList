01d6368aa0940ded0d6e8550fedf661cd65c41e5
anilsri.04@Gmail.com <anilsri.04@Gmail.com>
2019-05-04 10:55:26 +0530

    Demo Arraylist

--- ConsoleApp21/Program.cs
+++ ConsoleApp21/Program.cs
@@ -1,8 +1,7 @@
 ﻿using System;
+
+using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ConsoleApp21
 {
@@ -10,6 +9,26 @@ namespace ConsoleApp21
     {
         static void Main(string[] args)
         {
+            // create instance of array list
+            ArrayList list = new ArrayList();
+            // add the first name to the array list
+            list.Add("John Doe");
+            // add a new name to array list
+            list.Add("Jane Doe");
+            // add the name "Sometone Else"
+            list.Add("Someone Else");
+             list.Add(123);
+            list.Add(501.56);
+            list.Add(true);
+            list.Add(char.MaxValue);
+            // for loop
+            foreach (var name in list)
+            {
+                // print the name
+                Console.WriteLine(name);
+                // end 
+            }
+            Console.ReadLine();
         }
     }
 }
